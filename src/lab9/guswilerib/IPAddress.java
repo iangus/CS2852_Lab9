4442053b89218d2eca594f60f89c714bccde0373
Ian Guswiler <iangus97@gmail.com>
2016-05-10 14:49:33 -0500

    Partial completion of IPAddress

--- src/lab9/guswilerib/IPAddress.java
+++ src/lab9/guswilerib/IPAddress.java
@@ -1,5 +1,8 @@
 package lab9.guswilerib;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * CS2852 - 041
  * Spring 2016
@@ -8,4 +11,24 @@ package lab9.guswilerib;
  * Created: 5/10/2016
  */
 public class IPAddress {
+    private String address;
+
+    public IPAddress(String newAddress){
+        int counter = 0;
+        Scanner ipScanner = new Scanner(newAddress);
+        ipScanner.useDelimiter(".");
+        while(ipScanner.hasNext()){
+            try{
+                ipScanner.nextInt();
+                counter++;
+            } catch(InputMismatchException e){
+                throw new IllegalArgumentException("The IP address " + newAddress + " is not valid.");
+            }
+        }
+        if(counter != 4){
+            throw new IllegalArgumentException("The IP address " + newAddress + " is not valid.");
+        }
+        address = newAddress;
+    }
+
 }
